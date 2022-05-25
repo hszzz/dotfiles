668800a165ac43be940e3830955e03f27f67c430
hszzz <hszzz.me@gmail.com>
2022-05-25 16:19:21 +0800

    fix bugs

--- install.sh
+++ install.sh
@@ -120,10 +120,8 @@ install_oh_my_zsh() {
 }
 
 install_omz_plugins() {
-    cd ~/.oh-my-zsh/custom/plugins
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    cd ~
 }
 
 deb_install
@@ -135,4 +133,4 @@ install_universal_ctags
 install_config
 
 echo "-- Complete !!!"
-
+source ~/.zshrc
