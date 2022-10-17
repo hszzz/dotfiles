997077535d076339509f075fc42b545a6a1b4ecf
hszzz <hszzz.me@gmail.com>
2022-10-17 21:58:16 +0800

    fix

--- nvim/lua/lsp/null-ls.lua
+++ nvim/lua/lsp/null-ls.lua
@@ -1,7 +1,7 @@
 local status, null_ls = pcall(require, "null-ls")
 if not status then
-    vim.notify("没有找到 null-ls")
-    return
+	vim.notify("没有找到 null-ls")
+	return
 end
 
 local formatting = null_ls.builtins.formatting
@@ -9,18 +9,18 @@ local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
-    debug = false,
-    sources = {
-        formatting.shfmt,
-        formatting.stylua,
-        formatting.rustfmt,
-        formatting.clang_format,
-        formatting.black.with({ extra_args = { "--fast" } }),
+	debug = false,
+	sources = {
+		formatting.shfmt,
+		formatting.stylua,
+		formatting.rustfmt,
+		formatting.clang_format,
+		formatting.black.with({ extra_args = { "--fast" } }),
 
-        code_actions.gitsigns,
-    },
-    diagnostics_format = "[#{s}] #{m}",
-    on_attach = function(_)
-        vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()']])
-    end,
+		code_actions.gitsigns,
+	},
+	diagnostics_format = "[#{s}] #{m}",
+	on_attach = function(_)
+		vim.cmd([[ command! Format execute 'lua vim.lsp.buf.format { async = true }']])
+	end,
 })
