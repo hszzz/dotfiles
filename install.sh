27a00de5c379d6b89aa30520b28167600896395d
hszzz <hszzz.me@gmail.com>
2022-10-17 21:48:33 +0800

    fix conflicts

--- install.sh
+++ install.sh
@@ -83,15 +83,9 @@ install_oh_my_zsh() {
 }
 
 install_omz_plugins() {
-<<<<<<< HEAD
   rm -rf ~/.oh-my-zsh/custom/plugins/* 
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-=======
-	rm -rf ~/.oh-my-zsh/custom/plugins/*
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
->>>>>>> v1
 }
 
 install_oh_my_zsh
